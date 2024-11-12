6758123c4074722fb03d17aaf2873a11d7c4bd3e
yusraimran2 <yusraimran12@hotmail.com>
2024-11-12 21:24:00 +0500

    Add files via upload

---
+++ Q23.ts
@@ -0,0 +1,48 @@
+// 23:Conditionakl Tests:Write a series of conditional tests.Print a stsemment describing each test and your prediction for the results of each test.Your code should look something like this.
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// .Look closely at your results,and make sure you understand why each line evaluates to True or False.
+// .Create at least 10 tests.Have at least 5 test evaluate to True and another 5 tests evaluate to False.
+
+
+let phone = 'Samsung Galaxy';
+console.log("Is phone == 'Samsung Galaxy'? I predict True.")
+console.log(phone == 'Samsung Galaxy')
+
+let place = 'Canada';
+console.log("Is place == 'Canada'? I predict True.")
+console.log(place == 'Canada')
+
+let exam = 'Final Exam';
+console.log("Is exam == 'Final Exam'? I predict True.")
+console.log(exam == 'Final Exam')
+
+let car = 'Mustang Ford'
+console.log("Is car == 'Mustang Ford'? I predict True.")
+console.log(car == 'Mustang Ford')
+
+let drink = 'Cold Coffee';
+console.log("Is drink == 'Cold Coffee'? I predict True.")
+console.log(drink == 'Cold Coffee')
+
+let laptop = 'Hp';
+console.log("Is laptop == 'Hp'? I predict False.")
+console.log(laptop == 'Lenovo')
+
+let surf = 'Bonus';
+console.log("Is surf == 'Bonus'? I predict False.")
+console.log(surf == 'SurfExcel')
+
+let shampoo = 'Palmolive';
+console.log("Is shampoo == 'Palmolive'? I predict False.")
+console.log(shampoo == 'Sunsilk')
+
+let ketchup = 'National';
+console.log("Is ketchup == 'Natonal'? I predict False.")
+console.log(ketchup == 'Shingrella')
+
+let bag = 'Louis Vuitton';
+console.log("Is bag == 'Louis Vuitton'? I predict False.")
+console.log(bag == 'Gucci')
+
